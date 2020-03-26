b5ef9a496407f6706dbb85cc23192c8c4fbdc567
Ivo Gerner <i.gerner@outlook.com>
2020-03-26 01:32:13 +0100

    Created game board, gives valid play options for specified colour.

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,7 +1,15 @@
 package application;
 
+import reversi.Reversi;
+
 public class Main {
     public static void main(String[] argv) {
         System.out.println("Hello, world!");
+        Reversi reversi = new Reversi();
+        reversi.newGame();
+        reversi.printBoard(reversi.getBoard());
+        reversi.validMovesOverview(Reversi.WHITE_DISC);
+        reversi.getSuggestionsBoard();
+        reversi.printBoard(reversi.getSuggestionsBoard());
     }
 }

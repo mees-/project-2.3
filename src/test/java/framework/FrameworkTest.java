65222f1a54e2d532477fe7a81e05bb0b3dc525aa
JoeriRoijenga <roijenga.joeri@gmail.com>
2020-04-01 16:27:08 +0200

    Fix voor FrameworkTest.java

--- src/test/java/framework/FrameworkTest.java
+++ src/test/java/framework/FrameworkTest.java
@@ -2,6 +2,10 @@ package framework;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import connection.Connection;
+import framework.player.LocalConnectedPlayer;
+import framework.player.Player;
+import framework.player.RandomMovePlayer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
@@ -9,42 +13,26 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 
-import java.util.Random;
+import java.io.IOException;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class FrameworkTest {
 
     static Framework framework;
-    static String name;
 
     @BeforeAll
-    static void initializeFramework() {
-        framework = new Framework();
-        Random rand = new Random();
-        name = "test" + rand.nextInt(100);
-        framework.login(name);
-        framework.requestGameSync(GameType.TicTacToe);
+    static void initializeFramework() throws IOException {
+        Connection connection = new Connection();
+        Player player = new LocalConnectedPlayer(new RandomMovePlayer(), connection);
+        framework = new Framework(player, connection);
+        framework.login();
+        framework.runGameSync(GameType.TicTacToe);
+        framework.close();
     }
 
     @Test
     @Order(1)
-    public void Move() {
-        Move move = new Move(GameState.LocalTurn, 0, 1);
-        Move move1 = new Move(GameState.LocalTurn, 1, 0);
-
-        try {
-            assertEquals(GameState.RemoteTurn, framework.move(move));
-            assertEquals(GameState.LocalTurn, framework.move(move1));
-        } catch (InvalidMoveException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    @Order(2)
     public void State() {
-        assertEquals(name, framework.getState().getLocalUsername());
-        assertEquals(3, framework.getState().getBoard().getSize());
-        assertEquals(GameState.RemoteTurn, framework.getState().getGameState());
+        assertEquals(3, framework.getBoardSize());
     }
 }

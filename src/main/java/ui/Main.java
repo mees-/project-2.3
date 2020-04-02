cffa290af2111bde28fa4a3cee11f174d98e01d9
JoeriRoijenga <roijenga.joeri@gmail.com>
2020-04-02 12:26:24 +0200

    Basis main

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -1,7 +1,19 @@
 package ui;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+    public static void main(String[] args) { launch(args); }
+
+    @Override
+    public void start(Stage primaryStage) {
+        GridPane pane = new GridPane();
+        Scene scene = new Scene(pane);
+        primaryStage.setTitle("Tic Tac Toe"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show();
     }
 }

141909436a9727bb865a28fed10903847cb7abb6
Mees van Dijk <mees@mees.io>
2020-04-04 22:40:26 +0200

    Fix blockingplayer
    Since the match now checks whether the move was from the set, the blocking
    player now chooses the corresponding move from the set

--- src/main/java/framework/player/BlockingPlayer.java
+++ src/main/java/framework/player/BlockingPlayer.java
@@ -27,7 +27,13 @@ public class BlockingPlayer extends Player {
     @Override
     public Move getNextMove(BoardInterface board, Set<Move> possibleMoves) {
         try {
-            return move.take();
+            Move sourceMove = move.take();
+            for (Move possibleMove : possibleMoves) {
+                if (possibleMove.getX() == sourceMove.getX() && possibleMove.getY() == sourceMove.getY()) {
+                    return possibleMove;
+                }
+            }
+            throw new RuntimeException("The move from the async source wasn't found in the possible moves");
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }

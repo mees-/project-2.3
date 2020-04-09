077893faaa5b07eadfd380b8cd83ef078cd79ddf
Mees van Dijk <mees@mees.io>
2020-04-09 15:42:09 +0200

    Add subscribeCommand

--- src/main/java/connection/commands/ChallengeCommand.java
+++ src/main/java/connection/commands/ChallengeCommand.java
@@ -1,4 +1,29 @@
 package connection.commands;
 
-public class ChallengeCommand {
+import connection.commands.response.StandardResponse;
+import framework.GameType;
+
+public class ChallengeCommand extends Command<StandardResponse> {
+    private String player;
+    private GameType game;
+
+    public ChallengeCommand(String player, GameType game) {
+        this.player = player;
+        this.game = game;
+    }
+
+    @Override
+    public String getCommandString() {
+        return "challenge " + player + " " + game.toString();
+    }
+
+    @Override
+    public boolean isValidResponse(String[] response) {
+        return false;
+    }
+
+    @Override
+    public StandardResponse parseResponse(String[] response) {
+        return null;
+    }
 }

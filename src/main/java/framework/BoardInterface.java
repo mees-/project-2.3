b7fad1fc24e088b0025476a34d0eb19517b2b9a5
Mees van Dijk <mees@mees.io>
2020-04-11 19:59:38 +0200

    Add toString method to BoardInterface

--- src/main/java/framework/BoardInterface.java
+++ src/main/java/framework/BoardInterface.java
@@ -15,4 +15,28 @@ public abstract class BoardInterface {
     public abstract Set<Move> getValidMoves(GameState state);
 
     public abstract BoardInterface clone();
+
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        str.append("   ");
+
+        for (int x = 0; x < getSize() - 1; x++) {
+            str.append(x + " ");
+
+        }
+        str.append(getSize() - 1 + "\n");
+        for (int y = 0; y < getSize(); y++) {
+            str.append(y + " |");
+            for (int x = 0; x < getSize(); x++) {
+                str.append(getCell(x,y).toString() + "|");
+            }
+            str.append('\n');
+//            for (int size = 0; size < getSize() * 2 + 3; size++) {
+//                str.append('-');
+//            }
+//            str.append('\n');
+        }
+
+        return str.toString();
+    }
 }

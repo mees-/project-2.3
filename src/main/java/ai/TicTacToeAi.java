72fc9e6419939317b545121fc7daddeb6ff23ffe
Mees van Dijk <mees@mees.io>
2020-04-12 12:56:46 +0200

    Cleanup tictactoe AI

--- src/main/java/ai/TicTacToeAi.java
+++ src/main/java/ai/TicTacToeAi.java
@@ -13,11 +13,7 @@ public class TicTacToeAi extends Ai {
     @Override
     public void applyMoveToBoard(Move move, BoardInterface board) throws InvalidMoveException {
         if (board.getCell(move.getX(), move.getY()) == CellContent.Empty) {
-            try {
-                board.setCell(move.getX(), move.getY(), move.getPlayer().toCellContent());
-            } catch (GameState.InvalidOperationException e) {
-                throw new RuntimeException(e);
-            }
+            board.setCell(move.getX(), move.getY(), move.getPlayer().toCellContent());
         } else {
             throw new InvalidMoveException("That cell is already taken");
         }
@@ -27,47 +23,13 @@ public class TicTacToeAi extends Ai {
     public int analyzeMove(Move lastMove, BoardInterface board) {
         // this method was found on
         // https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-2-evaluation-function/
-        try {
-            CellContent gameWin = ((Board)board).checkForWinBetter();
-            if (gameWin == turn.toCellContent()) {
-                return 10;
-            } else if (gameWin == turn.otherPlayer().toCellContent()) {
-                return -10;
-            } else {
-                return 0;
-            }
-        } catch (GameState.InvalidOperationException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private boolean isMine(BoardInterface board, Collection<CoordinatePair> pairs) {
-        for (CoordinatePair pair : pairs) {
-            if (!isMine(board, pair)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean checkCells(BoardInterface board, Collection<Board.Cell> cells) {
-        for (Board.Cell cell : cells) {
-            if (!checkCell(board, cell)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean checkCell(BoardInterface board, Board.Cell cell) {
-        return board.getCell(cell.x, cell.y) == cell.content;
-     }
-
-    private boolean isMine(BoardInterface board, CoordinatePair pair) {
-        try {
-            return board.getCell(pair.x, pair.y) == turn.toCellContent();
-        } catch (GameState.InvalidOperationException e) {
-            throw new RuntimeException(e);
+        CellContent gameWin = ((Board)board).checkForWinBetter();
+        if (gameWin == turn.toCellContent()) {
+            return 10;
+        } else if (gameWin == turn.otherPlayer().toCellContent()) {
+            return -10;
+        } else {
+            return 0;
         }
     }
 

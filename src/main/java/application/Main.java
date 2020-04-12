97a2042dc5fa97cfb569b58a52d4ad65245c1e93
Mees van Dijk <mees@mees.io>
2020-04-12 03:22:41 +0200

    Add benchmark code

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -3,24 +3,38 @@ package application;
 import ai.TicTacToeAi;
 import connection.Connection;
 import framework.*;
-import framework.player.*;
+import tictactoe.Game;
 
 import java.io.IOException;
-import java.util.Random;
+import java.util.ArrayList;
 
 public class Main {
-    static Framework framework;
+    private static final ArrayList<GameState> results = new ArrayList<>();
+
+    private static final int amountOfGames = 10;
     public static void main(String[] argv) throws IOException, InterruptedException {
         System.out.println("Hello, world!");
-        Connection connection = new Connection();
-        LocalConnectedPlayer player = new LocalConnectedPlayer(new TicTacToeAi("mees" + (new Random()).nextInt(100)), connection);
-        framework = new Framework(player, connection);
-        connection.subscribe(GameType.TicTacToe);
-        Match test = framework.getMatchFuture().get();
-        test.waitForEnd();
-        player.logout().waitForResolve();
-        synchronized (System.out) {
-            framework.close();
+        for (int i = 0; i < amountOfGames; i++) {
+            Match match = new Match(new Game(),new TicTacToeAi("a"), new TicTacToeAi("b"));
+            match.setupGame(i % 2 == 0 ? GameState.TurnOne : GameState.TurnTwo);
+            match.onEnd(m -> {
+                synchronized (results) {
+                    results.add(m.getGameState());
+                    if (results.size() == amountOfGames) {
+                        results.notify();
+                    } else {
+                        System.out.println(results.size());
+                    }
+                }
+            });
+            match.startAsync();
         }
+        synchronized (results) {
+            results.wait();
+        }
+        System.out.println(results);
+        System.out.println("OneWin: " + results.stream().filter(r -> r == GameState.OneWin).count());
+        System.out.println("TwoWin: " + results.stream().filter(r -> r == GameState.TwoWin).count());
+        System.out.println("Draw: " + results.stream().filter(r -> r == GameState.Draw).count());
     }
 }
